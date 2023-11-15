a0724c223c144594a1896d3a36f1dad2b0789c3f
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-15 18:17:24 +0200

    init commit

---
+++ python-01-hello-world.py
@@ -0,0 +1,2 @@
+# Write the first python program. It will be: "Hello world!"
+print('Hello World!')
---
+++ python-02-string-concatenation.py
@@ -0,0 +1,5 @@
+# String concatenation
+name = 'John'
+age = 20
+
+print('Hello!\nMy name is ' + name + '.\nI`m ' + str(age) + ' years old!')
