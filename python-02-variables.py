6e9610dd5172956d26e9f237d31f7d7c0aee5cec
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-16 00:37:20 +0200

    init commit

---
+++ python-02-variables.py
@@ -0,0 +1,7 @@
+# Variables
+# Variable names can´t start with a number
+
+a = 5  # int
+b = 3.14  # float
+c = 'name'  # or "name" -> string
+d = True  # boolean
---
+++ python-03-string-print-input.py
@@ -0,0 +1,23 @@
+# String
+
+# String output/display
+print('Hello Python!')
+print("Hello Python!")
+print('-----------------')
+
+# To the new line
+print('Hello\nPython!')
+print('''Hello
+Python
+!''')
+print('-----------------')
+
+# Shielding
+print("Hello \"Python\"")
+print('Hello "Python"')
+print('-----------------')
+
+# String concatenation
+name = 'John'
+age = 20
+print('Hello!\nMy name is ' + name + '.\nI`m ' + str(age) + ' years old!')
