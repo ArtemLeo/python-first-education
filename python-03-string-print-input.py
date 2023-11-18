07b2a44a392a5a6d9a7f6e8eb0b6d5e7b5077d50
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-18 04:22:37 +0200

    add python-05-calculator.py

--- python-03-string-print-input.py
+++ python-03-string-print-input.py
@@ -23,7 +23,18 @@ age = 20
 print('Hello!\nMy name is ' + name + '.\nI`m ' + str(age) + ' years old!')
 print('-----------------')
 
-# input
+# Multiplication by string
+string = 'Hello'
+print(string * 5)
+
+name = input('What is your name?: ')
+count = input('How many times need to show this information?: ')
+print('Your name is ' + name + '!')
+print('And I show this information ' + count + ' times:\n' + name * int(count))
+print('-----------------')
+
+# input()
 name = input('Enter your name: ')
 age = input('How old are you: ')
 print('Your name is ' + name + '.\nYou are ' + age + ' yeas old!\nWelcome ' + name + '!')
+print('-----------------')
