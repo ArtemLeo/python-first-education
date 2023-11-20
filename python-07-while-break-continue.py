de0a6822facc04a73b1bf09ce6aa0580101a43f3
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-20 06:16:33 +0200

    add python-07-while-break-continue.py

---
+++ python-07-while-break-continue.py
@@ -0,0 +1,29 @@
+# Loops: while
+# Operators: break, continue
+
+# while
+x = 1
+while x <= 5:
+    print('x = ' + str(x))
+    x += 1
+print('----------')
+
+# break
+y = 15
+count = 1
+while True:
+    print('while "True", count = ' + str(count))
+    count += 1
+    y -= 1
+    if y <= 1:
+        break
+print('----------')
+
+# continue
+i = 0
+while i <= 10:
+    i += 1
+    if i % 2 == 0:
+        print(i)
+    else:
+        continue
