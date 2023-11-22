8307f380946ece95874f4f04107dac6c4d1ffff0
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-22 11:00:44 +0200

    add python-10-own-methods.py

--- python-10-own-methods.py
+++ python-10-own-methods.py
@@ -1,15 +1,16 @@
 # Own methods
+# Operator "return"
 
-# Create method (without arguments)
+# create method (without arguments)
 def show_info():
     print('Displayed some information!')
 
 
-# Use current method
+# use current method
 show_info()
 print('---------------------------')
 
-# Create method (with arguments)
+# create method (with arguments)
 number = float(input('Enter any number: '))
 
 
@@ -17,6 +18,32 @@ def show_input_num(value):
     print('Your input is ' + str(value))
 
 
-# Use current method
+# use current method
 show_input_num(number)
 print('---------------------------')
+
+
+# operator "return"
+def multiply(value1, value2):
+    return value1 * value2
+
+
+# use current method
+result = multiply(2, 2)
+print('Result of multiply method = ' + str(result))
+print('---------------------------')
+
+
+# create method min_max
+def show_max_number(num1, num2):
+    if num1 > num2:
+        return num1
+    elif num2 > num1:
+        return num2
+
+
+# use current method
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+result = show_max_number(x, y)
+print('Result of "show_max_number" method = ' + str(result))
