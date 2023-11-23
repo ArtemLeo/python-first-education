dc058624bf7562f35848029f970b6a7d85363b5e
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-23 12:33:45 +0200

    add python-10-own-methods.py

--- python-10-own-methods.py
+++ python-10-own-methods.py
@@ -1,34 +1,40 @@
 # Own methods
 # Operator "return"
+# """ Documentation comment """
 
 # create method (without arguments)
 def show_info():
+    """Method show_info"""
     print('Displayed some information!')
 
 
 # use current method
+print(show_info.__doc__)
 show_info()
 print('---------------------------')
 
+
 # create method (with arguments)
-number = float(input('Enter any number: '))
-
-
 def show_input_num(value):
+    """Method show_input_num"""
     print('Your input is ' + str(value))
 
 
 # use current method
+print(show_input_num.__doc__)
+number = float(input('Enter any number: '))
 show_input_num(number)
 print('---------------------------')
 
 
 # operator "return"
 def multiply(value1, value2):
+    """Method multiply"""
     return value1 * value2
 
 
 # use current method
+print(multiply.__doc__)
 result = multiply(2, 2)
 print('Result of multiply method = ' + str(result))
 print('---------------------------')
@@ -36,6 +42,7 @@ print('---------------------------')
 
 # create method min_max
 def show_max_number(num1, num2):
+    """Method show_max_number"""
     if num1 > num2:
         return num1
     elif num2 > num1:
@@ -43,6 +50,7 @@ def show_max_number(num1, num2):
 
 
 # use current method
+print(show_max_number.__doc__)
 x = int(input('Enter first number: '))
 y = int(input('Enter second number: '))
 result = show_max_number(x, y)
