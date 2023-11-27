825143a4a9b9714e4d1b555d4014044cd12e47c3
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-27 11:23:15 +0200

    add python-12-exceptions.py

---
+++ python-12-exceptions.py
@@ -0,0 +1,10 @@
+# Import
+# Random
+
+import random
+from random import *
+from random import randint
+from random import randint as own_method_name
+
+for i in range(10):
+    print(randint(1, 100))
