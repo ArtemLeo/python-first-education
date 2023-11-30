d9ade0077a358bfde7ee19a763241af3601d58fe
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-30 12:07:38 +0200

    add python-12-exceptions.py

---
+++ python-02-data-types.py
@@ -0,0 +1,13 @@
+# Data types
+# Names can´t start with a some number
+
+a = 5  # int (integer number)
+b = 3.14  # float (double number)
+c = 'name'  # str (string type) or " "
+d = True  # bool (boolean type) or False
+
+e = (5, 'hello', 10.5)  # tup (tuple) -> (ordered, immutable type)
+f = [5, 'hey', 10.5]  # list (ordered, mutable type)
+g = {'a', 'b', 'c'}  # set (unordered type, unique objects)
+h = {'key1': 'value', "key2": "value"}  # dict (dictionary) -> (unordered type -> key : value)
+
