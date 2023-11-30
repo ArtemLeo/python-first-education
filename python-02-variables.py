251130883cb7aacf10cfc5e94a43a78f1653b9f0
Artem Andriushchenko <link.by.artem@gmail.com>
2023-11-30 11:57:28 +0200

    add python-12-exceptions.py

--- python-02-variables.py
+++ python-02-variables.py
@@ -1,7 +1,12 @@
 # Variables
 # Variable names can´t start with a number
 
-a = 5  # int
-b = 3.14  # float
-c = 'name'  # or "name" -> string
-d = True  # boolean
+a = 5  # int (integer number)
+b = 3.14  # float (double number)
+c = 'name'  # or "name" -> string (string type)
+d = True  # bool (boolean type)
+
+e = [5, 'hey', 10.5]  # list (ordered, mutable type)
+f = (5, 'hello', 10.5)  # tup (ordered, immutable type)
+g = {'key1': 'value', "key2": "value"}  # dict (unordered type -> key : value)
+h = {'a', 'b', 'c'}  # set (unordered type, unique objects)
