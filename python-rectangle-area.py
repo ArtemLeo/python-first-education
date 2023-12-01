25b8a8131016f4a2dfbddb6357050bb340954e21
Artem Andriushchenko <link.by.artem@gmail.com>
2023-12-01 17:28:06 +0200

    add python-12-exceptions.py

---
+++ python-rectangle-area.py
@@ -0,0 +1,9 @@
+# Find rectangle area
+def rectangle_area(value1, value2):
+    return 'Rectangle area = ' + str(value1 * value2)
+
+
+height = 4
+weight = 5
+result = rectangle_area(height, weight)
+print(result)
