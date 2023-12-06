09f263201b788432956bfdcf512de2e5985e0762
Artem Andriushchenko <link.by.artem@gmail.com>
2023-12-07 01:34:47 +0200

    add python-12-exceptions.py

--- python-07-random-import.py
+++ python-07-random-import.py
@@ -1,10 +1,9 @@
 # Import
-# Random
-
 import random
 from random import *
 from random import randint
 from random import randint as own_method_name
 
+# Random
 for i in range(10):
     print(randint(1, 100))
